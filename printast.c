c5156a53816679c9c888ac334f125912ef91d680
Stormalf <stormalf.dev@gmail.com>
2025-12-13 11:24:15 +0100

    adding missing nodes in printast

--- printast.c
+++ printast.c
@@ -64,20 +64,57 @@ static const char kTypeKindStr[17][8] = {
 };
 
 
-static const char kNodeKindStr[76][21] = {
-    "NULL_EXPR",  "ADD",     "SUB",       "MUL",       "DIV",       "NEG",
-    "MOD",        "BITAND",  "BITOR",     "BITXOR",    "SHL",       "SHR",
-    "EQ",         "NE",      "LT",        "LE",        "ASSIGN",    "COND",
-    "COMMA",      "MEMBER",  "ADDR",      "DEREF",     "NOT",       "BITNOT",
-    "LOGAND",     "LOGOR",   "RETURN",    "IF",        "FOR",       "DO",
-    "SWITCH",     "CASE",    "BLOCK",     "GOTO",      "GOTO_EXPR", "LABEL",
-    "LABEL_VAL",  "FUNCALL", "EXPR_STMT", "STMT_EXPR", "VAR",       "VLA_PTR",
-    "NUM",        "CAST",    "MEMZERO",   "ASM",       "CAS",       "EXCH",
-    "CAS_N",      "EXCH_N",  "LOAD",      "LOAD_N",    "STORE",     "STORE_N",
-    "TESTANDSET", "TESTANDSETA",  "CLEAR", "RELEASE",    "FETCHADD",     "FETCHSUB", 
-    "FETCHXOR", "FETCHAND",  "FETCHOR", "SUBFETCH",    "SYNC",     "BUILTIN_MEMCPY",  
-    "BUILTIN_MEMSET", "BUILTIN_CLZ",  "BUILTIN_CTZ", "POPCOUNT",    "EXPECT",     "RETURN_ADDR",     
-    "BUILTIN_ADD_OVERFLOW", "BUILTIN_SUB_OVERFLOW",  "BUILTIN_MUL_OVERFLOW", "UNREACHABLE",  
+static const char kNodeKindStr[442][21] = {
+    "NULL_EXPR", "ADD", "SUB", "MUL", "DIV", "NEG", "MOD", "BITAND",  "BITOR", "BITXOR", "SHL", "SHR", "EQ", "NE",
+    "LT", "LE", "ASSIGN", "COND", "COMMA", "MEMBER", "ADDR", "DEREF", "NOT", "BITNOT", "LOGAND", "LOGOR", "RETURN",
+    "IF", "FOR", "DO", "SWITCH", "CASE", "BLOCK", "GOTO", "GOTO_EXPR", "LABEL", "LABEL_VAL",  "FUNCALL", "EXPR_STMT", "STMT_EXPR", "VAR", "VLA_PTR", "NUM", "CAST", "MEMZERO",
+    "ASM", "CAS", "EXCH", "CAS_N", "EXCH_N", "LOAD", "LOAD_N", "STORE", "STORE_N", "TESTANDSET", "TESTANDSETA", "CLEAR", "RELEASE", "FETCHADD", "FETCHSUB", "FETCHXOR", "FETCHAND",  
+    "FETCHOR", "SUBFETCH", "SYNC", "BUILTIN_MEMCPY",  "BUILTIN_MEMSET", "BUILTIN_CLZ", "BUILTIN_CTZ", "POPCOUNT",    
+    "RETURN_ADDR", "BUILTIN_ADD_OVERFLOW", "BUILTIN_SUB_OVERFLOW",  "BUILTIN_MUL_OVERFLOW", "UADD_OVERFLOW",
+    "UADDL_OVERFLOW", "UADDLL_OVERFLOW", "UMUL_OVERFLOW", "UMULL_OVERFLOW", "UMULLL_OVERFLOW",
+    "BSWAP16", "BSWAP32", "BSWAP64", "ALLOCA", "ABORT", "EXPECT", "EMMS", "LFENCE", "MFENCE",
+    "SFENCE", "PAUSE", "STMXCSR", "LDMXCSR", "CVTPI2PS", "CVTPS2PI", "CLFLUSH", "VEC_INIT_V2SI",
+    "VEC_EXT_V2SI", "VEC_EXT_V4SI", "VEC_INIT_V8QI", "VEC_INIT_V8QI", "PACKSSWB", "PACKSSDW",
+    "PACKUSWB", "PUNPCKHBW", "PUNPCKHWD", "PUNPCKHDQ", "PUNPCKLBW", "PUNPCKLWD", "PUNPCKLDQ",
+    "PADDB", "PADDW", "PADDD", "PADDQ", "PADDSB", "PADDSW", "PADDUSB", "PADDUSW", "PSUBB", 
+    "PSUBW", "PSUBD", "PSUBQ", "PSUBSB", "PSUBSW", "PSUBUSB", "PSUBUSW", "PMADDWD", "PMULHW", 
+    "PMULLW", "PSLLW", "PSLLWI", "PSLLD", "PSLLDI", "PSLLQ", "PSLLQI", "PSRAW", "PSRAWI", "PSRAD",
+    "PSRADI", "PSRLW", "PSRLWI", "PSRLD", "PSRLDI", "PSRLQ", "PSRLQI", "PAND", "PANDN", "POR", 
+    "PXOR", "PCMPEQB", "PCMPGTB", "PCMPEQW", "PCMPEQD", "PCMPGTW", "PCMPGTD", "ADDSS", "SUBSS", "MULSS", 
+    "DIVSS", "SQRTSS",  "SQRTPS",  "RSQRTSS", "RSQRTPS", "RCPSS",  "RCPPS", "MINSS", "MINPS", 
+    "MAXPS", "MAXSS", "ANDPS", "ANDNPS", "ORPS", "XORPS", "CMPEQSS", "CMPLTSS", "CMPLESS", "MOVSS",
+    "CMPNEQSS", "CMPNLTSS", "CMPNLESS", "CMPORDSS", "CMPUNORDSS", "CMPEQPS", "CMPLTPS", "CMPLEPS",
+    "CMPGTPS", "CMPGEPS", "CMPNEQPS", "CMPNLTPS", "CMPNLEPS", "CMPNGTPS", "CMPNGEPS", "CMPORDPS",
+    "CMPUNORDPS", "COMIEQ", "COMILT", "COMILE", "COMIGT", "COMIGE", "COMINEQ", "UCOMIEQ", "UCOMILT",
+    "UCOMILE", "UCOMIGT", "UCOMIGE", "UCOMINEQ", "CVTSS2SI", "CVTSS2SI64", "CVTTSS2SI", "CVTTSS2SI64",
+    "CVTTPS2PI", "CVTSI2SS", "CVTSI642SS", "MOVLHPS", "MOVHLPS", "UNPCKHPS", "UNPCKLPS", "LOADHPS",
+    "STOREHPS", "LOADLPS", "STORELPS", "MOVMSKPS", "SHUFPS", "SHUFFLE", "PMAXSW", "PMAXUB", "PMINSW",
+    "PMINUB", "PMOVMSKB", "PMULHUW", "MASKMOVQ", "PAVGB", "PAVGW", "PSADBW", "MOVNTQ", "MOVNTPS", "SHUFPD", 
+    "ADDSD", "SUBSD", "MULSD", "DIVSD", "SQRTPD", "SQRTSD", "MOVSD", "MINPD", "MINSD", "MAXPD", "MAXSD", 
+    "ANDPD", "ANDNPD", "ORPD", "XORPD", "CMPEQPD", "CMPLTPD", "CMPLEPD", "CMPGTPD", "CMPGEPD", "CMPNEQPD", 
+    "CMPNLTPD", "CMPNLEPD", "CMPNGTPD", "CMPNGEPD", "CMPORDPD", "CMPUNORDPD", "CMPEQSD", "CMPLTSD", 
+    "CMPLESD", "CMPNEQSD", "CMPNLTSD", "CMPNLESD", "CMPORDSD", "CMPUNORDSD", "COMISDEQ", "COMISDLT", 
+    "COMISDLE", "COMISDGT", "COMISDGE", "COMISDNEQ", "UCOMISDEQ", "UCOMISDLT", "UCOMISDLE", "UCOMISDGT", 
+    "UCOMISDGE", "UCOMISDNEQ", "MOVQ128", "CVTDQ2PD", "CVTDQ2PS", "CVTPD2DQ", "CVTPD2PI", "CVTPD2PS",
+    "CVTTPD2DQ", "CVTTPD2PI", "CVTPI2PD","CVTPS2DQ", "CVTTPS2DQ", "CVTPS2PD", "CVTSD2SI", "CVTSD2SI64",
+    "CVTTSD2SI", "CVTTSD2SI64", "CVTSD2SS", "CVTSI2SD", "CVTSI642SD", "CVTSS2SD", "UNPCKHPD", "UNPCKLPD",
+    "LOADHPD", "LOADLPD", "MOVMSKPD", "PACKSSWB128", "PACKSSDW128", "PACKUSWB128", "PUNPCKHB128", "PUNPCKHWD128",
+    "PUNPCKHDQ128", "PUNPCKHQDQ128", "PUNPCKLBW128", "PUNPCKLWD128", "PUNPCKLDQ128", "PUNPCKLQDQ128",
+    "PADDSB128", "PADDSW128", "PADDUSB128", "PADDUSW128", "PSUBSB128", "PSUBSW128", "PSUBUSB128", "PSUBUSW128", 
+    "PMADDWD128", "PMULHW128", "PMULUDQ", "PMULUDQ128", "PSLLWI128", "PSLLDI128", "PSLLQI128", "PSRAWI128",
+    "PSRADI128", "PSRLWI128", "PSRLDI128", "PSRLQI128", "PSLLW128", "PSLLD128", "PSLLQ128", "PSRAW128", 
+    "PSRAD128", "PSRLW128", "PSRLD128", "PSRLQ128", "PANDN128", "PMAXSW128", "PMAXUB128", "PMINSW128", "PMINUB128",
+    "PMOVMSKB128", "PMULHUW128", "MASKMOVDQU", "PAVGB128", "PAVGW128", "PSADBW128", "MOVNTI", "MOVNTI64",
+    "MOVNTDQ", "MOVNTPD", "PARITY", "PARITYL", "PARITYLL", "MWAIT", "MONITOR", "ADDSUBPS", "HADDPS", "HSUBPS", 
+    "MOVSHDUP", "MOVSLDUP", "ADDSUBPD", "HADDPD", "HSUBPD", "PHADDW128", "PHADDD128", "PHADDSW128", 
+    "PHADDW", "PHADDD", "PHADDSW", "PHSUBW128", "PHSUBD128", "PHSUBSW128", "PHSUBW", "PHSUBD", "PHSUBSW", 
+    "PMADDUBSW128", "PMADDUBSW", "PMULHRSW128", "PMULHRSW", "PSHUFB128", "PSHUFB", "PSIGNB128", "PSIGNW128", 
+    "PSIGND128", "PSIGNB", "PSIGNW",  "PSIGND", "PABSB128", "PABSW128", "PABSD128",  "PABSB", "PABSW", "PABSD", 
+    "PTESTZ128", "PTESTC128", "PTESTNZC128", "PBLENDVB128", "BLENDVPS", "BLENDVPD", "PMINSB128", "PMAXSB128", 
+    "PMINUW128", "PMAXUW128", "PMINSD128", "PMAXSD128", "PMINUD128", "PMAXUD128", "PMULDQ128", "PHMINPOSUW128",
+    "PMOVSXBD128", "PMOVSXWD128", "PMOVSXBQ128", "PMOVSXDQ128", "PMOVSXWQ128", "PMOVSXBW128",  "PMOVZXBD128", 
+    "PMOVZXWD128", "PMOVZXBQ128", "PMOVZXDQ128", "PMOVZXWQ128", "PMOVZXBW128", "PACKUSDW128", "MOVNTDQA", 
+    "CRC32QI", "CRC32HI", "CRC32SI", "CRC32DI", "PSHUFD", "UNREACHABLE",  
 };
 
 
@@ -194,7 +231,23 @@ static void PrintType(FILE *f, int l, const char *s, Type *t) {
       PrintBool(f, l + 2, "is_packed: ", t->is_packed);
       PrintBool(f, l + 2, "is_aligned: ", t->is_aligned);
       PrintBool(f, l + 2, "is_const: ", t->is_const);
+      PrintBool(f, l + 2, "is_restrict: ", t->is_restrict);
+      PrintBool(f, l + 2, "is_volatile: ", t->is_volatile);
       PrintBool(f, l + 2, "is_static: ", t->is_static);
+      PrintBool(f, l + 2, "is_ms_abi: ", t->is_ms_abi);
+      PrintBool(f, l + 2, "is_pointer: ", t->is_pointer);
+      PrintBool(f, l + 2, "has_vla: ", t->has_vla);
+      PrintBool(f, l + 2, "is_weak: ", t->is_weak);
+      PrintBool(f, l + 2, "is_inline: ", t->is_inline);
+      PrintBool(f, l + 2, "is_compound_lit: ", t->is_compound_lit);
+      PrintBool(f, l + 2, "is_vector: ", t->is_vector);
+      PrintBool(f, l + 2, "is_constructor: ", t->is_constructor);
+      PrintInt(f, l + 2, "constructor_priority: ", t->constructor_priority);
+      PrintBool(f, l + 2, "is_destructor: ", t->is_destructor);
+      PrintInt(f, l + 2, "destructor_priority: ", t->destructor_priority);
+      PrintStr(f, l + 2, "visibility: ", t->visibility);
+      PrintStr(f, l + 2, "alias_name: ", t->alias_name);
+      PrintStr(f, l + 2, "section: ", t->section);
       PrintType(f, l + 2, "return_ty: ", t->return_ty);
       PrintType(f, l + 2, "params: ", t->params);
       PrintBool(f, l + 2, "is_variadic: ", t->is_variadic);
@@ -226,37 +279,53 @@ static void PrintNode(FILE *f, int l, const char *s, Node *n) {
     if (n->kind >= 0 && n->kind < (int)(sizeof(kNodeKindStr)/sizeof(kNodeKindStr[0])))
       kindstr = kNodeKindStr[n->kind];
 
-
     PrintLine(f, l, "%sNode { # %p", s, n);
     PrintLine(f, l + 2, "kind: ND_%s", kindstr);
     PrintLine(f, l + 2, "kind: ND_%s", kNodeKindStr[n->kind]);
     PrintType(f, l + 2, "ty: ", n->ty);
-    PrintNode(f, l + 2, "lhs: ", n->lhs);
-    PrintNode(f, l + 2, "rhs: ", n->rhs);
-    PrintNode(f, l + 2, "cond: ", n->cond);
-    PrintNode(f, l + 2, "then: ", n->then);
-    PrintNode(f, l + 2, "els: ", n->els);
-    PrintNode(f, l + 2, "init: ", n->init);
-    PrintNode(f, l + 2, "inc: ", n->inc);
-    PrintNode(f, l + 2, "body: ", n->body);
+    if (n->lhs) PrintNode(f, l + 2, "lhs: ", n->lhs);
+    if (n->rhs) PrintNode(f, l + 2, "rhs: ", n->rhs);
+    if (n->cond) PrintNode(f, l + 2, "cond: ", n->cond);
+    if (n->then) PrintNode(f, l + 2, "then: ", n->then);
+    if (n->els) PrintNode(f, l + 2, "els: ", n->els);
+    if (n->init) PrintNode(f, l + 2, "init: ", n->init);
+    if (n->inc) PrintNode(f, l + 2, "inc: ", n->inc);
+    if (n->body) PrintNode(f, l + 2, "body: ", n->body);
+    if (n->case_next) PrintNode(f, l + 2, "case_next: ", n->case_next);
+    if (n->default_case) PrintNode(f, l + 2, "default_case: ", n->default_case);
     PrintType(f, l + 2, "func_ty: ", n->func_ty);
     PrintNode(f, l + 2, "args: ", n->args);
     PrintObj(f, l + 2, "ret_buffer: ", n->ret_buffer);
     PrintBool(f, l + 2, "pass_by_stack: ", n->pass_by_stack);
     PrintBool(f, l + 2, "realign_stack: ", n->realign_stack);
-    PrintNode(f, l + 2, "case_next: ", n->case_next);
-    PrintNode(f, l + 2, "default_case: ", n->default_case);
-    PrintStr(f, l + 2, "label: ", n->label);
-    PrintStr(f, l + 2, "unique_label: ", n->unique_label);
-    PrintNode(f, l + 2, "goto_next: ", n->goto_next);
-    PrintStr(f, l + 2, "brk_label: ", n->brk_label);
-    PrintStr(f, l + 2, "cont_label: ", n->cont_label);
+    if (n->label) PrintStr(f, l + 2, "label: ", n->label);
+    if (n->unique_label) PrintStr(f, l + 2, "unique_label: ", n->unique_label);
+    if (n->goto_next) PrintNode(f, l + 2, "goto_next: ", n->goto_next);
+    if (n->brk_label) PrintStr(f, l + 2, "brk_label: ", n->brk_label);
+    if (n->cont_label) PrintStr(f, l + 2, "cont_label: ", n->cont_label);
     PrintInt(f, l + 2, "begin: ", n->begin);
-    PrintAsm(f, l + 2, "asm: ", n->asm_str);
+    if (n->asm_str) PrintAsm(f, l + 2, "asm: ", n->asm_str);
     PrintInt(f, l + 2, "end: ", n->end);
-    PrintMember(f, l + 2, "member: ", n->member);
+    if (n->member) PrintMember(f, l + 2, "member: ", n->member);
     PrintObj(f, l + 2, "var: ", n->var);
     PrintInt(f, l + 2, "val: ", n->val);
+    PrintInt(f, l + 2, "memorder: ", n->memorder);
+    if (n->cas_addr) PrintNode(f, l + 2, "cas_addr: ", n->cas_addr);
+    if (n->cas_old) PrintNode(f, l + 2, "cas_old: ", n->cas_old);
+    if (n->cas_new) PrintNode(f, l + 2, "cas_new: ", n->cas_new);
+    if (n->cas_ptr) PrintNode(f, l + 2, "cas_ptr: ", n->cas_ptr);
+    if (n->cas_expected) PrintNode(f, l + 2, "cas_expected: ", n->cas_expected);
+    if (n->cas_desired) PrintNode(f, l + 2, "cas_desired: ", n->cas_desired);
+    if (n->cas_weak) PrintNode(f, l + 2, "cas_weak: ", n->cas_weak);
+    if (n->cas_success) PrintNode(f, l + 2, "cas_success: ", n->cas_success);
+    if (n->cas_failure) PrintNode(f, l + 2, "cas_failure: ", n->cas_failure);
+    if (n->builtin_dest) PrintNode(f, l + 2, "builtin_dest: ", n->builtin_dest);
+    if (n->builtin_src) PrintNode(f, l + 2, "builtin_src: ", n->builtin_src);
+    if (n->builtin_size) PrintNode(f, l + 2, "builtin_size: ", n->builtin_size);
+    if (n->atomic_expr) PrintNode(f, l + 2, "atomic_expr: ", n->atomic_expr);
+    if (n->atomic_addr) PrintObj(f, l + 2, "atomic_addr: ", n->atomic_addr);
+    PrintBool(f, l + 2, "atomic_fetch: ", n->atomic_fetch);
+    PrintBool(f, l + 2, "is_scalar_promoted: ", n->is_scalar_promoted);
     if (n->fval) PrintLine(f, l + 2, "fval: %Lf", n->fval);
     PrintLine(f, l, "}");
   }
@@ -276,28 +345,36 @@ static void PrintObj(FILE *f, int l, const char *s, Obj *o) {
   if (!o) return;
   PrintLine(f, l, "%sObj { # %p", s, o);
   PrintStr(f, l + 2, "name: ", o->name);
+  if (o->funcname) PrintStr(f, l + 2, "funcname: ", o->funcname);
+  if (o->alias_name) PrintStr(f, l + 2, "alias_name: ", o->alias_name);
   PrintType(f, l + 2, "ty: ", o->ty);
   PrintBool(f, l + 2, "is_local: ", o->is_local);
+  PrintBool(f, l + 2, "is_compound_lit: ", o->is_compound_lit);
   PrintInt(f, l + 2, "align: ", o->align);
   PrintInt(f, l + 2, "offset: ", o->offset);
+  PrintInt(f, l + 2, "order: ", o->order);
+  PrintInt(f, l + 2, "nbparm: ", o->nbparm);
   PrintBool(f, l + 2, "is_function: ", o->is_function);
   PrintBool(f, l + 2, "is_definition: ", o->is_definition);
   PrintBool(f, l + 2, "is_static: ", o->is_static);
   PrintBool(f, l + 2, "is_weak: ", o->is_weak);
+  PrintBool(f, l + 2, "is_extern: ", o->is_extern);
   PrintBool(f, l + 2, "is_externally_visible: ", o->is_externally_visible);
-  PrintStr(f, l + 2, "asmname: ", o->asmname);
-  PrintStr(f, l + 2, "section: ", o->section);
-  PrintStr(f, l + 2, "visibility: ", o->visibility);
+  if (o->asmname) PrintStr(f, l + 2, "asmname: ", o->asmname);
+  if (o->section) PrintStr(f, l + 2, "section: ", o->section);
+  if (o->visibility) PrintStr(f, l + 2, "visibility: ", o->visibility);
   PrintBool(f, l + 2, "is_tentative: ", o->is_tentative);
   //PrintBool(f, l + 2, "is_string_literal: ", o->is_string_literal);
   PrintBool(f, l + 2, "is_tls: ", o->is_tls);
-  PrintStr(f, l + 2, "init_data: ", o->init_data);
+  if (o->init_data) PrintStr(f, l + 2, "init_data: ", o->init_data);
   PrintRelo(f, l + 2, "rel: ", o->rel);
   PrintBool(f, l + 2, "is_inline: ", o->is_inline);
   PrintBool(f, l + 2, "is_aligned: ", o->is_aligned);
   PrintBool(f, l + 2, "is_noreturn: ", o->is_noreturn);
   PrintBool(f, l + 2, "is_destructor: ", o->is_destructor);
+  PrintInt(f, l + 2, "destructor_priority: ", o->destructor_priority);
   PrintBool(f, l + 2, "is_constructor: ", o->is_constructor);
+  PrintInt(f, l + 2, "constructor_priority: ", o->constructor_priority);
   PrintBool(f, l + 2, "is_externally_visible: ", o->is_externally_visible);
   PrintBool(f, l + 2,
             "is_no_instrument_function: ", o->is_no_instrument_function);
@@ -305,14 +382,22 @@ static void PrintObj(FILE *f, int l, const char *s, Obj *o) {
             "is_force_align_arg_pointer: ", o->is_force_align_arg_pointer);
   PrintBool(f, l + 2,
             "is_no_caller_saved_registers: ", o->is_no_caller_saved_registers);
+  PrintBool(f, l + 2, "vfork_used: ", o->vfork_used);            
   PrintInt(f, l + 2, "stack_size: ", o->stack_size);
+  PrintInt(f, l + 2, "overflow_arg_area: ", o->overflow_arg_area);
+  PrintBool(f, l + 2, "pass_by_stack: ", o->pass_by_stack);
   PrintObj(f, l + 2, "params: ", o->params);
-  PrintNode(f, l + 2, "body: ", o->body);
-  PrintObj(f, l + 2, "locals: ", o->locals);
-  PrintObj(f, l + 2, "va_area: ", o->va_area);
-  PrintObj(f, l + 2, "alloca_bottom: ", o->alloca_bottom);
+  if (o->body) PrintNode(f, l + 2, "body: ", o->body);
+  if (o->locals) PrintObj(f, l + 2, "locals: ", o->locals);
+  if (o->va_area) PrintObj(f, l + 2, "va_area: ", o->va_area);
+  if (o->alloca_bottom) PrintObj(f, l + 2, "alloca_bottom: ", o->alloca_bottom);
   PrintBool(f, l + 2, "is_live: ", o->is_live);
   PrintBool(f, l + 2, "is_root: ", o->is_root);
+  PrintInt(f, l + 2, "file_no: ", o->file_no);
+  PrintInt(f, l + 2, "line_no: ", o->line_no);
+  PrintBool(f, l + 2, "is_prototyped: ", o->is_prototyped);
+  PrintBool(f, l + 2, "is_address_used: ", o->is_address_used);
+  PrintBool(f, l + 2, "is_param: ", o->is_param);
   PrintLine(f, l, "}");
 }
 
